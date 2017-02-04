e4c830348599280bb6fc7ec1596a6e7427fd14e8
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-02-04 00:20:06 -0500

    Implement a polynomial.

---
+++ command/polynomial.go
@@ -0,0 +1,50 @@
+package command
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+// Polynomial represents a polynomial defined on a finite field.
+type Polynomial struct {
+	Field        *Field
+	Dimension    int
+	Coefficients []*big.Int
+	Const        *big.Int
+}
+
+// NewPolynomial creates a new random polynomial on the given finite field.
+// The dimension of the polynomial is the given dim, and it has a given
+// constant.
+func NewPolynomial(field *Field, c *big.Int, dim int) (*Polynomial, error) {
+
+	coeffs := make([]*big.Int, dim)
+	for i := 0; i < dim; i++ {
+		v, err := rand.Int(rand.Reader, field.Max)
+		if err != nil {
+			return nil, err
+		}
+		coeffs[i] = v
+	}
+
+	return &Polynomial{
+		Field:        field,
+		Dimension:    dim,
+		Coefficients: coeffs,
+		Const:        c,
+	}, nil
+
+}
+
+// Call computes a value F(x) where x is the given number.
+func (p *Polynomial) Call(x *big.Int) *big.Int {
+
+	res := big.NewInt(0)
+	for i, a := range p.Coefficients {
+		y := new(big.Int).Exp(x, big.NewInt(int64(i+1)), p.Field.Prime)
+		res.Add(res, y.Mul(y, a))
+	}
+	res.Add(res, p.Const)
+	return res.Mod(res, p.Field.Prime)
+
+}
---
+++ command/polynomial_test.go
@@ -0,0 +1,30 @@
+package command
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestPolynomial(t *testing.T) {
+
+	field := NewField(big.NewInt(37))
+	threshold := 2
+	polynomial, err := NewPolynomial(field, big.NewInt(0), threshold-1)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if len(polynomial.Coefficients) != 1 {
+		t.Fatal("Dimension of the polynomial is wrong:", polynomial)
+	}
+
+	var res *big.Int
+	if res = polynomial.Call(big.NewInt(0)); res.Int64() != 0 {
+		t.Error("F(0) returns a wrong value:")
+	}
+
+	if res = polynomial.Call(big.NewInt(1)); res.Cmp(polynomial.Coefficients[0]) != 0 {
+		t.Error("F(1) returns a wrong value:", res)
+	}
+
+}
