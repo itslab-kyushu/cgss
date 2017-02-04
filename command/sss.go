5fe635da8bc235cf019d01ca0fc4e8252faf4faa
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-02-04 04:50:37 -0500

    Distribute takes an array of byte instead of big.Int.

--- command/sss.go
+++ command/sss.go
@@ -1,56 +1,85 @@
 package command
 
 import (
+	"crypto/rand"
 	"fmt"
+	"math"
 	"math/big"
 )
 
 // Share defines a share of Shamir's Secret Sharing scheme.
 type Share struct {
 	Key   *big.Int
-	Value *big.Int
+	Value []*big.Int
 	Field *Field
 }
 
-// Distribute computes shares made by a prime number and a secret.
-func Distribute(prime *big.Int, value *big.Int, size, threshold int) (shares []Share, err error) {
+// Distribute computes shares having a given secret.
+func Distribute(secret []byte, chankByte, size, threshold int) (shares []Share, err error) {
 
-	field := NewField(prime)
-	polynomial, err := NewPolynomial(field, value, threshold-1)
+	prime, err := rand.Prime(rand.Reader, chankByte*8)
 	if err != nil {
 		return
 	}
+	field := NewField(prime)
 
+	nvalue := int(math.Ceil(float64(len(secret)) / float64(chankByte)))
 	shares = make([]Share, size)
 	for i := range shares {
 		key := big.NewInt(int64(i + 1))
 		shares[i] = Share{
 			Key:   key,
-			Value: polynomial.Call(key),
+			Value: make([]*big.Int, nvalue),
 			Field: field,
+		}
+	}
+
+	var value *big.Int
+	for chank := 0; chank < nvalue; chank++ {
+		if len(secret) > chankByte {
+			value = new(big.Int).SetBytes(secret[:chankByte])
+			secret = secret[chankByte:]
+		} else {
+			value = new(big.Int).SetBytes(secret)
+			secret = nil
+		}
+
+		polynomial, err := NewPolynomial(field, value, threshold-1)
+		if err != nil {
+			return nil, err
+		}
+
+		for i := range shares {
+			key := big.NewInt(int64(i + 1))
+			shares[i].Value[chank] = polynomial.Call(key)
 		}
 
 	}
 
 	return
-
 }
 
 // Reconstruct computes the secret value from a set of shares.
-func Reconstruct(shares []Share) (res *big.Int, err error) {
+func Reconstruct(shares []Share) (bytes []byte, err error) {
 
 	if len(shares) == 0 {
 		err = fmt.Errorf("No shares are given")
 		return
 	}
 
-	res = big.NewInt(0)
-	field := shares[0].Field
-	for i, s := range shares {
-		res.Add(res, new(big.Int).Mul(s.Value, beta(field, shares, i)))
-	}
-	res.Mod(res, field.Prime)
+	bytes = []byte{}
+	for chank := 0; chank < len(shares[0].Value); chank++ {
 
+		value := big.NewInt(0)
+		field := shares[0].Field
+		for i, s := range shares {
+			value.Add(value, new(big.Int).Mul(s.Value[chank], beta(field, shares, i)))
+		}
+		value.Mod(value, field.Prime)
+
+		bytes = append(bytes, value.Bytes()...)
+
+	}
 	return
 
 }
--- command/sss_test.go
+++ command/sss_test.go
@@ -1,30 +1,52 @@
 package command
 
-import (
-	"math/big"
-	"testing"
-)
+import "testing"
 
 func TestSS1(t *testing.T) {
 
 	var err error
 
-	prime := big.NewInt(37)
-	secret := big.NewInt(int64(35))
+	secret := "abcdefghijklmnopqrstuvwxyz"
 
 	size := 10
 	threshold := 3
 
-	shares, err := Distribute(prime, secret, size, threshold)
+	shares, err := Distribute([]byte(secret), 8, size, threshold)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
+	t.Log(shares)
 
 	res, err := Reconstruct(shares[:threshold])
 	if err != nil {
 		t.Error(err.Error())
 	}
-	if res.Cmp(secret) != 0 {
+	if string(res) != secret {
+		t.Error("SS1 is broken:", res, secret)
+	}
+
+}
+
+func TestSS1Word(t *testing.T) {
+
+	var err error
+
+	secret := "a"
+
+	size := 10
+	threshold := 3
+
+	shares, err := Distribute([]byte(secret), 256, size, threshold)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Log(shares)
+
+	res, err := Reconstruct(shares[:threshold])
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if string(res) != secret {
 		t.Error("SS1 is broken:", res, secret)
 	}
 
