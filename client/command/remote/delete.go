8a068929b563258c94edd0b10b99ebe22e917d3d
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-02-08 03:09:53 -0500

    Add license comments.

--- client/command/remote/delete.go
+++ client/command/remote/delete.go
@@ -1,3 +1,24 @@
+//
+// client/command/remote/delete.go
+//
+// Copyright (c) 2017 Junpei Kawamoto
+//
+// This file is part of cgss.
+//
+// cgss is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// cgss is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with cgss.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 package remote
 
 import (
